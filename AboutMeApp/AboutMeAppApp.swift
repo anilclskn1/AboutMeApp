5f7abb35ea9e0c30f1c37af1573cf03d50fd631f
Anil Caliskan <anilclskn.1@gmail.com>
2024-02-12 11:14:30 +0300

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Anil Caliskan on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
